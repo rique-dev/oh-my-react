b850a92dcb7aac892e7f8b8477c199c09259aec3
Henrique Rodrigues <henryqrm@gmail.com>
2018-07-05 22:43:45 -0300

    init Login Page

--- src/client/routes/Login/LoginPage.tsx
+++ src/client/routes/Login/LoginPage.tsx
@@ -1,20 +1,14 @@
-import { FormattedMessage, Helmet, Link, React, renderRoutes } from '@lib'
-import Radar from '@Login/Radar'
-
-import messages from './LoginMessages'
-import s from './LoginStyle.scss'
+import { Background } from '@components'
+import { SignUp } from '@containers'
+import { Fragment, Helmet, React } from '@lib'
 
 export default ({ route }: any) => (
-  <div>
+  <Fragment>
     <Helmet>
       <title>Login</title>
     </Helmet>
-    <h1 className={s.h1}>
-      <FormattedMessage {...messages.hello} />
-    </h1>
-    <li>
-      <Link to={Radar.to({ id: 123 })}>com params</Link>
-    </li>
-    {renderRoutes(route.routes)}
-  </div>
+    <Background type="Shape">
+      <SignUp />
+    </Background>
+  </Fragment>
 )
