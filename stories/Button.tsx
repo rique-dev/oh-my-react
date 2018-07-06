6ca253befc4342cb394246eee57fc747002ba963
Henrique Rodrigues <henryqrm@gmail.com>
2018-07-05 22:48:20 -0300

    story changes

---
+++ stories/Button.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+export const Button = ({ children }: React.Props<any>) => (
+  <button>{children}</button>
+)
--- stories/IndexStory.tsx
+++ stories/IndexStory.tsx
@@ -1,31 +1,30 @@
-import { action } from '@storybook/addon-actions'
-import { storiesOf } from '@storybook/react'
-import * as React from 'react'
+// import { storiesOf } from '@storybook/react'
+// import * as React from 'react'
 
-import { Button } from '@storybook/react/demo'
+// import { Button } from './Button'
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍
-      </span>
-    </Button>
-  ))
+// storiesOf('Button', module)
+//   .add('with text', () => (
+//     <Button>Hello Button</Button>
+//   ))
+//   .add('with some emoji', () => (
+//     <Button>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍
+//       </span>
+//     </Button>
+//   ))
 
-storiesOf('UI|Button/Add', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍
-      </span>
-    </Button>
-  ))
+// storiesOf('UI|Button/Add', module)
+//   .add('with text', () => (
+//     <Button>Hello Button</Button>
+//   ))
+//   .add('with some emoji', () => (
+//     <Button>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍
+//       </span>
+//     </Button>
+//   ))
 
-storiesOf('asda', module).add('asd', () => <div>asd</div>)
+// storiesOf('asda', module).add('asd', () => <div>asd</div>)
