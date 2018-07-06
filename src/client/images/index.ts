ed0bee26efc1517a1c406ae697bac6a2e989e2af
Henrique Rodrigues <henryqrm@gmail.com>
2018-07-05 22:38:33 -0300

    add images

--- src/client/images/index.ts
+++ src/client/images/index.ts
@@ -1,3 +1,6 @@
 // tslint:disable no-require-imports no-var-requires
-export const logo = require('./react_logo.svg')
+export const reactLogo = require('./react_logo.svg')
+export const itsLogo = require('./its4apps.svg')
+export const its4 = require('./its4.svg')
+export const luiz = { src: require('./luiz.jpg'), alt: 'Luiz Gama' }
 // tslint:disable no-require-imports no-var-requires
