c02baf01c81d661be2aeb6b23d746cc1b29a4eb5
Henrique Rodrigues <henryqrm@gmail.com>
2018-07-05 22:47:28 -0300

    component Header

---
+++ src/client/containers/Header/Header.scss.d.ts
@@ -0,0 +1,5 @@
+export const scope: string
+export const menu: string
+export const logo: string
+export const wrapperAvatar: string
+export const avatar: string
---
+++ src/client/containers/Header/Header.tsx
@@ -0,0 +1,54 @@
+import { Avatar, IconButton, LogoIts4 } from '@components'
+import Home from '@Home'
+import { luiz } from '@images'
+import { bind, Link, React } from '@lib'
+import { Grid } from '@lib/material-ui'
+import s from './Header.scss'
+
+export default class Header extends React.Component {
+  @bind
+  clickButtonMenu(e: React.MouseEvent<HTMLElement>): void {
+    this.setState({}) // Envia para redux
+  }
+
+  render(): JSX.Element {
+    return (
+      <Grid
+        container
+        justify="space-between"
+        wrap="nowrap"
+        direction="row"
+        className={s.scope}
+      >
+        <Grid item container wrap="nowrap" xs={2}>
+          <Grid item>
+            <div className={s.menu}>
+              <IconButton
+                handleClick={this.clickButtonMenu}
+                color="#fff"
+                nameIcon="menu"
+              />
+            </div>
+          </Grid>
+          <Grid item>
+            <div className={s.logo}>
+              <Link to={Home.path}>
+                <LogoIts4 color="white" />
+              </Link>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid item container justify="flex-end" xs={10}>
+          <Grid item xs={11} container justify="flex-end" alignItems="center">
+            asd
+          </Grid>
+          <Grid item>
+            <div className={s.wrapperAvatar}>
+              <Avatar className={s.avatar} src={luiz.src} alt={luiz.alt} />
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+}
